fbfe31cc56920d63d4010601130a2e770d3027cb
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-04 10:31:54 -0400

    Changes made

---
+++ assignment.py
@@ -0,0 +1,101 @@
+# 1. Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+# 1
+x[1][0] = 15
+# print(x)
+
+# 2
+students[0]["last_name"] = "Bryant"
+# print(students)
+
+# 3
+sports_directory["soccer"][0] = "Andres"
+# print(sports_directory)
+
+# 4
+z[0]["y"] = 30
+print(z)
+
+
+# 2. Iterate Through a List of Dictionaries
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+
+def iterateDictionary(names):
+    for student in names:
+        for key, val in student.items():
+            print(f"{key} - {val}")
+
+
+iterateDictionary(students) 
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+# 3. Get Values From a List of Dictionaries
+
+def iterateDictionary2(key_name, some_list):
+    for student in some_list:
+        print(student[key_name])
+
+iterateDictionary2('first_name', students)
+
+iterateDictionary2('last_name', students)
+
+
+# 4. Iterate Through a Dictionary with List Values
+
+def printInfo(some_dict):
+    for each_key in some_dict:
+        print(f"{len(each_key)} {each_key.upper()}")
+        for index in each_key:
+            print(each_key[index])
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
